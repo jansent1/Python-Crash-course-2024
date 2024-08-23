108b0a57db9d9ebd4f32024fe09d42e47a69bc68
Teun Jansen <teunjansen91@gmail.com>
2024-08-23 21:06:31 +0200

    caesar parameters implemented

--- free_code_camp/scientific_computing_python/cypher/cypher.py
+++ free_code_camp/scientific_computing_python/cypher/cypher.py
@@ -5,18 +5,18 @@ shift = 3
 
 # If I'm correct i can use a parameter to enter the different texts into the caesar function
 
-def caesar():
+def caesar(message, offset):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     encrypted_text = ''
 
-    for char in text_2.lower():
+    for char in message.lower():
         if char == ' ':
             # If the char is a space we can skip it by adding 1 to char to reach the next char:
             encrypted_text += char
         else:
             index = alphabet.find(char)
             # Make sure the X, Y and Z are linked to the remainder/beginning of the alphabet:
-            new_index = (index + shift) % len(alphabet)
+            new_index = (index + offset) % len(alphabet)
             # Update encrypted text to char + 3:
             encrypted_text += alphabet[new_index]
     # Print the original text:
@@ -24,4 +24,4 @@ def caesar():
     # Print only the encrypted text:        
     print('encrypted text:', encrypted_text)
 
-caesar()
+caesar(text_1, shift)
