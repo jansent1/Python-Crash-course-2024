d00b61cf60c469b25f35af2765d5445eb5fc8a70
Teun Jansen <teunjansen91@gmail.com>
2024-08-25 20:55:37 +0200

    case converter untill step 8

--- free_code_camp/scientific_computing_python/4. case_converter/main.py
+++ free_code_camp/scientific_computing_python/4. case_converter/main.py
@@ -5,3 +5,12 @@ In this project, you'll write a program that takes a string formatted in Camel C
 
 The project has two phases: first you'll use a for loop to implement the program. Then you'll learn how to use List Comprehension instead of a loop to achieve the same results.
 """
+
+def convert_to_snake_case(pascal_or_camel_cased_string):
+    snake_cased_char_list = []
+    for char in pascal_or_camel_cased_string:
+        if char.isupper():
+            converted_character = '_' + char.lower()
+            snake_cased_char_list.append(converted_character)
+        else:
+           snake_cased_char_list.append(char)
