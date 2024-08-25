5a48b13ccede32c8ca7193119384bb240f1a7159
Teun Jansen <teunjansen91@gmail.com>
2024-08-25 21:27:14 +0200

    case converter phase 2 start untill step 16

--- free_code_camp/scientific_computing_python/4. case_converter/main.py
+++ free_code_camp/scientific_computing_python/4. case_converter/main.py
@@ -8,15 +8,6 @@ The project has two phases: first you'll use a for loop to implement the program
 
 def convert_to_snake_case(pascal_or_camel_cased_string):
     snake_cased_char_list = []
-    for char in pascal_or_camel_cased_string:
-        if char.isupper():
-            converted_character = '_' + char.lower()
-            snake_cased_char_list.append(converted_character)
-        else:
-           snake_cased_char_list.append(char)
-    snake_cased_string = ''.join(snake_cased_char_list)
-    clean_snake_cased_string = snake_cased_string.strip('_')    # Removes the first underscore added after the convertion above. (this was extra if a PascalCase was supplied as argument)
-    return clean_snake_cased_string
 
 def main():
     print(convert_to_snake_case('aLongAndComplexString'))
