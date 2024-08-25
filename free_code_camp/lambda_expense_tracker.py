2124b2043915c14a32c1c2f53f2f02cc70f13501
Teun Jansen <teunjansen91@gmail.com>
2024-08-25 14:27:15 +0200

    expense_tracker step 15

--- free_code_camp/lambda_expense_tracker.py
+++ free_code_camp/lambda_expense_tracker.py
@@ -1,2 +1,22 @@
+# Lists contain fields with data. my_list[1, 2, 3] would be a list of numbers
+# Dictionaries contain records with key: value pairs. my_dict{'key1': value, 'key2': value}
+# Lambda functions are brief, anonymous functions in Python, ideal for simple, one-time tasks. They are defined by the lambda keyword.  test = lambda x: x * 2
+
+# test = lambda x: x * 2
+# print(list(map(test, [2, 3, 5, 8])))
+
+# replace the list function with the sum() to get the total amount of expenses:
+# test = lambda x: x * 2
+# print(sum(map(test, [2,3,5,8])))
+
 def add_expense(expenses, amount, category):
-    pass
+    expenses.append({ 'amount': amount, 'category': category })
+
+def print_expenses(expenses):
+    for expense in expenses:
+        print(f'Amount: {expense["amount"]}, Category: {expense["category"]}')
+
+def total_expenses(expenses):
+    pass    # see lambda functions!
+
+expenses = []
