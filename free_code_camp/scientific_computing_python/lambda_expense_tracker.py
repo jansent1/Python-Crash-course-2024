e8f6cc9666e4853098be0594e928aab5a1627e5f
Teun Jansen <teunjansen91@gmail.com>
2024-08-25 20:15:09 +0200

    lambda intro untill step 31

--- free_code_camp/scientific_computing_python/lambda_expense_tracker.py
+++ free_code_camp/scientific_computing_python/lambda_expense_tracker.py
@@ -19,4 +19,19 @@ def print_expenses(expenses):
 def total_expenses(expenses):
     return sum(map(lambda expense: expense['amount'], expenses))   # see lambda functions in Google docs!
 
-expenses = []
+def filter_expenses_by_category(expenses, category):
+   return filter(lambda expense: expense['category'] == category, expenses)
+
+def main():
+    expenses = []
+    while True:
+        print('\nExpense Tracker')
+        print('1. Add an expense')
+        print('2. List all expenses')
+        print('3. Show total expenses')
+        print('4. Filter expenses by category')
+        print('5. Exit')
+        
+        choice = input('Enter your choice: ')
+        if choice == '1':
+            amount = input('Enter amount: ')
