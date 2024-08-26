6386ee835c110e28728f6aaa856c69f7aa987be0
Teun Jansen <teunjansen91@gmail.com>
2024-08-26 11:19:48 +0200

    arithmetic_arranger start

---
+++ free_code_camp/scientific_computing_python/6. arithmetic formatter project/main.py
@@ -0,0 +1,15 @@
+def arithmetic_arranger(problems, show_answers=False):
+
+    # 1. Check the length of the parameter problems:
+    if len(problems) > 5:
+        return 'Error: Too many problems.'
+    
+    # 2. Check the operant:
+
+    # 3. Check for non-digits:
+
+    # 4. Check max-digits of the operands: 
+
+    return problems
+
+print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
