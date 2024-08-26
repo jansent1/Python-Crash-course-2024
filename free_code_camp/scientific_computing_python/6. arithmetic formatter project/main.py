e2ca8ad5ff7ec381b68d4a5c9903a37414bdb4cb
Teun Jansen <teunjansen91@gmail.com>
2024-08-26 13:42:33 +0200

    arithmetic_arranger errors and checks

--- free_code_camp/scientific_computing_python/6. arithmetic formatter project/main.py
+++ free_code_camp/scientific_computing_python/6. arithmetic formatter project/main.py
@@ -5,10 +5,28 @@ def arithmetic_arranger(problems, show_answers=False):
         return 'Error: Too many problems.'
     
     # 2. Check the operant:
-
+    operators = []
+    for problem in problems:
+        array = problem.split()
+        operators.append(array[1])
+    for operator in operators:
+        if operator in ['*', '/']:
+            return "Error: Operator must be '+' or '-'."
     # 3. Check for non-digits:
+    numbers = []
+    for problem in problems:
+        array = problem.split()
+        numbers.append(array[0])
+        numbers.append(array[2])
+        # print(numbers)
 
-    # 4. Check max-digits of the operands: 
+    for number in numbers:
+        if not number.isdigit():
+            return 'Error: Numbers must only contain digits.'
+    # 4. Check max-digits of the operands:
+        elif len(number) > 4:
+            'Error: Numbers cannot be more than four digits.'
+ 
 
     return problems
 
