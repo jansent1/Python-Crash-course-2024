4c72653f723fd1fbc3beab09027ef730b9e3dda7
Teun Jansen <teunjansen91@gmail.com>
2024-08-30 10:38:54 +0200

    basic dictionary operations

--- free_code_camp/scientific_computing_python/8. shortest path algorithm design/main.py
+++ free_code_camp/scientific_computing_python/8. shortest path algorithm design/main.py
@@ -4,3 +4,30 @@ copper = {
           }
 
 print(copper['species'])
+
+# add a new key/value pair:
+copper['food'] = 'hay'
+print(copper)
+
+# reassign a value:
+copper['species'] = 'Cavia porcellus'
+print(copper)
+
+# itterate over the KEYS in a dict:
+for i in copper:
+    print(i)
+
+# itterate over the VALUES in a dict:
+for i in copper.values():
+    print(i)
+
+# itterate over the KEYS/VALUE PAIRS in a dict (stored in tuples):
+for i in copper.items():
+    print(i)
+
+# And to print the key value pairs as strings:
+for i, j in copper.items():
+    print(i, j)
+
+# Remove a key/value pair:
+del copper['age']
