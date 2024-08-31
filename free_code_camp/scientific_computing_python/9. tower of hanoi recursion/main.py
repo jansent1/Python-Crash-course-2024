f5a59c33af7e266e7dbac05abe1f59f7cb45b400
Teun Jansen <teunjansen91@gmail.com>
2024-08-31 11:50:34 +0200

    tower of hanoi setup untill step 7

---
+++ free_code_camp/scientific_computing_python/9. tower of hanoi recursion/main.py
@@ -0,0 +1,18 @@
+
+# A, B, C represent the rods and the lists with numbers represent the disks:
+rods = {
+    'A': list(range(3, 0, -1)), # Generate a sequence of number counting down
+    'B': [],
+    'C': []
+}
+
+"""
+The goal of the Tower of Hanoi is moving all the disks to the last rod. To do that, you must follow three simple rules:
+
+You can move only top-most disks
+You can move only one disk at a time
+You cannot place larger disks on top of smaller ones
+"""
+
+def move():
+    pass
