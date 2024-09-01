e6624719345c702eacf2893cb20c52fe097c2613
Teun Jansen <teunjansen91@gmail.com>
2024-09-01 17:25:25 +0200

    tower of hanoi recursion method finished

--- free_code_camp/scientific_computing_python/9. tower of hanoi recursion/main.py
+++ free_code_camp/scientific_computing_python/9. tower of hanoi recursion/main.py
@@ -15,10 +15,25 @@ You cannot place larger disks on top of smaller ones
 The Tower of Hanoi puzzle can be solved in 2**n - 1 moves, where n is the number of disks.
 """
 
-# Move Algorithm using RECURSION:
+NUMBER_OF_DISKS = 5
+A = list(range(NUMBER_OF_DISKS, 0, -1))
+B = []
+C = []
+
 def move(n, source, auxiliary, target):
-    # display starting configuration
-    print(rods, '\n')
-    
-# initiate call from source A to target C with auxiliary B:
-move(NUMBER_OF_DISKS, 'A', 'B', 'C')
+    if n <= 0:
+        return
+    # move n - 1 disks from source to auxiliary, so they are out of the way
+    move(n - 1, source, target, auxiliary)
+        
+    # move the nth disk from source to target
+    target.append(source.pop())
+        
+    # display our progress
+    print(A, B, C, '\n')
+        
+    # move the n - 1 disks that we left on auxiliary onto target
+    move(n - 1,  auxiliary, source, target)
+              
+# initiate call from source A to target C with auxiliary B
+move(NUMBER_OF_DISKS, A, B, C)
