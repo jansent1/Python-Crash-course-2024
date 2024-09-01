a64316a79ddfe3f4ca56386e7c609e2f025f802e
Teun Jansen <teunjansen91@gmail.com>
2024-09-01 10:22:57 +0200

    tower of hanoi untill step 29 using itteration

--- free_code_camp/scientific_computing_python/9. tower of hanoi recursion/main.py
+++ free_code_camp/scientific_computing_python/9. tower of hanoi recursion/main.py
@@ -17,8 +17,32 @@ You cannot place larger disks on top of smaller ones
 The Tower of Hanoi puzzle can be solved in 2**n - 1 moves, where n is the number of disks.
 """
 
+
+# Move Algorithm using ITTERATION:
 def move(n, source, auxiliary, target):
+    # display starting configuration
     print(rods)
+    for i in range(number_of_moves):
+        remainder = (i + 1) % 3
+        if remainder == 1:
+            print(f'Move {i + 1} allowed between {source} and {target}')
+            forward = False
+            if not rods[target]:
+                forward = True
+            elif rods[source] and rods[source][-1] < rods[target][-1]:
+                forward = True
+            if forward:
+                print(f'Moving disk {rods[source][-1]} from {source} to {target}')
+                rods[target].append(rods[source].pop())
+            else:
+                print(f'Moving disk {rods[target][-1]} from {target} to {source}')
+                rods[source].append(rods[target].pop())
+            # display our progress:
+            print(rods)
+        elif remainder == 2:
+            print(f'Move {i + 1} allowed between {source} and {auxiliary}')
+        elif remainder == 0:
+            print(f'Move {i + 1} allowed between {auxiliary} and {target}')
 
 
 # initiate call from source A to target C with auxiliary B:
