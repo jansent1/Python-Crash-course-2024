a3cb7f6907d974fed1ac9c4ee5c0fdc9a0a8cc33
Teun Jansen <teunjansen91@gmail.com>
2024-09-02 07:10:57 +0200

    Merge sort. left_part while loop

--- free_code_camp/scientific_computing_python/10. merge sort algorithm/main.py
+++ free_code_camp/scientific_computing_python/10. merge sort algorithm/main.py
@@ -29,4 +29,6 @@ def merge_sort(array):
             right_array_index += 1
         sorted_index += 1
     while left_array_index < len(left_part):
-        pass
+        array[sorted_index] = left_part[left_array_index]
+        left_array_index += 1
+        sorted_index += 1
