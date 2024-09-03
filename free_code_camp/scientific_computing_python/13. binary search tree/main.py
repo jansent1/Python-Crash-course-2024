0c03ea9046a727532e57f29cd6a41299beb90c13
Teun Jansen <teunjansen91@gmail.com>
2024-09-03 09:56:25 +0200

    BST untill step 12

---
+++ free_code_camp/scientific_computing_python/13. binary search tree/main.py
@@ -0,0 +1,14 @@
+class TreeNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+    def _insert(self, node, key):
+        if node is None:
+            return TreeNode(key)
+        if key < node.key:
+            pass
