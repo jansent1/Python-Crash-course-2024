47de49e416aad6b68a5e2aad059ab3c30817aa13
Teun Jansen <teunjansen91@gmail.com>
2024-09-03 11:25:29 +0200

    BST search method finished (step 26)

--- free_code_camp/scientific_computing_python/13. binary search tree/main.py
+++ free_code_camp/scientific_computing_python/13. binary search tree/main.py
@@ -7,6 +7,7 @@ class TreeNode:
 class BinarySearchTree:
     def __init__(self):
         self.root = None
+
     def _insert(self, node, key):
         if node is None:
             return TreeNode(key)
@@ -17,3 +18,12 @@ class BinarySearchTree:
         return node
     def insert(self, key):
         self.root = self._insert(self.root, key)
+
+    def _search(self, node, key):
+        if node is None or node.key == key:
+            return node
+        if key < node.key:
+            return self._search(node.left, key)
+        return self._search(node.right, key)
+    def search(self,key):
+        return self._search(self.root, key)
