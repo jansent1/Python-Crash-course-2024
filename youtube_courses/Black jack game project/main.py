98bc904d52ec1d61485aa39c78ef0f462dc8c4f0
Teun Jansen <teunjansen91@gmail.com>
2024-09-06 21:11:02 +0200

    Correct doctypes added before start

--- youtube_courses/Black jack game project/main.py
+++ youtube_courses/Black jack game project/main.py
@@ -1,7 +1,22 @@
 """
 DOCTYPE:
-This will be a game implementing everything I've learned and recaped about Python today.
+This will be a game implementing everything I've learned and can recap about Python today.
 The link to the video is: https://www.youtube.com/watch?v=eWRfhZUzrAc&list=PLWKjhJtqVAbnqBxcdjVGgT3uVR10bzTEB
 time stamp for project start: 3:27:18
+
+Check all the different versions on the github page because we are going to do a lot of refactoring for this project.
+1. Beginning
+2. Deck Class
+3. Card Class
+4. Hand Class
+5. Game Class
+6. Testing
+
+and ofcourse cording to the description and message for each merge if a significant part of the program is finished.
 """
 
+# 1. 
+suit = "hearts"
+rank = "K"
+value = 10
+print("Your card is: " + rank)
