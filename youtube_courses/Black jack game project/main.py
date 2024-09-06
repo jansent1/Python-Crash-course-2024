eba3b1b4745e6ebb6fa00b31cf9622a676e5867c
Teun Jansen <teunjansen91@gmail.com>
2024-09-06 20:39:41 +0200

    start blackjack game

---
+++ youtube_courses/Black jack game project/main.py
@@ -0,0 +1,7 @@
+"""
+DOCTYPE:
+This will be a game implementing everything I've learned and recaped about Python today.
+The link to the video is: https://www.youtube.com/watch?v=eWRfhZUzrAc&list=PLWKjhJtqVAbnqBxcdjVGgT3uVR10bzTEB
+time stamp for project start: 3:27:18
+"""
+
