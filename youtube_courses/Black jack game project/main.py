075d1886b8085132abc27aadbb1206309028fa45
Teun Jansen <teunjansen91@gmail.com>
2024-09-07 20:13:48 +0200

    beginning first refactoring. ranks list

--- youtube_courses/Black jack game project/main.py
+++ youtube_courses/Black jack game project/main.py
@@ -19,7 +19,25 @@ import random
 # 1. 
 cards = []
 suits = ["spades", "clubs", "heart", "diamonds"]
-ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+# Notice the update to the rank list containing now dictionaries:
+ranks = [
+    {"rank": "A", "value": 11},
+    {"rank": "2", "value": 2},
+    {"rank": "3", "value": 3},
+    {"rank": "4", "value": 4},
+    {"rank": "5", "value": 5},
+    {"rank": "6", "value": 6},
+    {"rank": "7", "value": 7},
+    {"rank": "8", "value": 8},
+    {"rank": "9", "value": 9},
+    {"rank": "10", "value": 10},
+    {"rank": "J", "value": 10},
+    {"rank": "Q", "value": 10},
+    {"rank": "K", "value": 10},
+]
+
+# fill the empty cards list with the deck and values stored in the ranks list:
 for suit in suits:
     for rank in ranks:
         cards.append([suit, rank])
@@ -37,3 +55,9 @@ def deal(number):
     return cards_dealt
 
 shuffle()
+
+# get the value of each rank without using an if statement. Instead we'll store both rankName and value in the ranks list using dictionaries.
+
+card = deal(1)[0]   # deal() returns a list. in this case containing one item since number = 1. [0] allows us to acces that first item.
+
+print(card)
