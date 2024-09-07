3555bb4f918a48153728ee9ef1a99810e9ef0c4a
Teun Jansen <teunjansen91@gmail.com>
2024-09-07 20:27:53 +0200

    2. Start of the Deck class

--- youtube_courses/Black jack game project/main.py
+++ youtube_courses/Black jack game project/main.py
@@ -16,50 +16,51 @@ and ofcourse cording to the description and message for each merge if a signific
 """
 import random
 
-# 1. 
-cards = []
-suits = ["spades", "clubs", "heart", "diamonds"]
+class Deck:
+    # 2. 
+    cards = []
+    suits = ["spades", "clubs", "heart", "diamonds"]
 
-# Notice the update to the rank list containing now dictionaries:
-ranks = [
-    {"rank": "A", "value": 11},
-    {"rank": "2", "value": 2},
-    {"rank": "3", "value": 3},
-    {"rank": "4", "value": 4},
-    {"rank": "5", "value": 5},
-    {"rank": "6", "value": 6},
-    {"rank": "7", "value": 7},
-    {"rank": "8", "value": 8},
-    {"rank": "9", "value": 9},
-    {"rank": "10", "value": 10},
-    {"rank": "J", "value": 10},
-    {"rank": "Q", "value": 10},
-    {"rank": "K", "value": 10},
-]
+    # Notice the update to the rank list containing now dictionaries:
+    ranks = [
+        {"rank": "A", "value": 11},
+        {"rank": "2", "value": 2},
+        {"rank": "3", "value": 3},
+        {"rank": "4", "value": 4},
+        {"rank": "5", "value": 5},
+        {"rank": "6", "value": 6},
+        {"rank": "7", "value": 7},
+        {"rank": "8", "value": 8},
+        {"rank": "9", "value": 9},
+        {"rank": "10", "value": 10},
+        {"rank": "J", "value": 10},
+        {"rank": "Q", "value": 10},
+        {"rank": "K", "value": 10},
+    ]
 
-# fill the empty cards list with the deck and values stored in the ranks list:
-for suit in suits:
-    for rank in ranks:
-        cards.append([suit, rank])
+    # fill the empty cards list with the deck and values stored in the ranks list:
+    for suit in suits:
+        for rank in ranks:
+            cards.append([suit, rank])
 
-def shuffle():
-    # use the random library to shuffle the list of cards:
-    random.shuffle(cards)
+    def shuffle():
+        # use the random library to shuffle the list of cards:
+        random.shuffle(cards)
 
 
-def deal(number):
-    """ Create an empty list and pop of the last item in cards. Append that item the empty list """
-    cards_dealt = []
-    for x in range(number):
-        card = cards.pop()
-        cards_dealt.append(card)
-    return cards_dealt
+    def deal(number):
+        """ Create an empty list and pop of the last item in cards. Append that item the empty list """
+        cards_dealt = []
+        for x in range(number):
+            card = cards.pop()
+            cards_dealt.append(card)
+        return cards_dealt
 
-shuffle()
+    shuffle()
 
-"""get the value of each rank without using an if statement. Instead we'll store both rankName and value in the ranks list using dictionaries."""
+    """get the value of each rank without using an if statement. Instead we'll store both rankName and value in the ranks list using dictionaries."""
 
-card = deal(1)[0]   # deal() returns a list. in this case containing one item since number = 1. [0] allows us to acces that first item.
+    card = deal(1)[0]   # deal() returns a list. in this case containing one item since number = 1. [0] allows us to acces that first item.
 
-print(card[1]["value"])
-print(card)
+    print(card[1]["value"])
+    # print(card)
