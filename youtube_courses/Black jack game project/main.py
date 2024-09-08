6e6b5989f40c0f33753e242b3fab8b98190ccef3
Teun Jansen <teunjansen91@gmail.com>
2024-09-08 19:04:02 +0200

    5. second part of the Game class

--- youtube_courses/Black jack game project/main.py
+++ youtube_courses/Black jack game project/main.py
@@ -139,6 +139,33 @@ class Game:
             for i in range(2):
                 player_hand.add_card(deck.deal(1))
                 dealer_hand.add_card(deck.deal(1))
+    # Print the GUI:
+            print()
+            print("*" * 30)
+            print(f"Game {game_number} of {games_to_play}")
+            print("*" * 30)
+            player_hand.display()
+            dealer_hand.display()
+
+    # Check for a winner:
+    def check_winner(self, player_hand, dealer_hand):
+        if player_hand.get_value() > 21:
+            print("You busted. Dealer wins!")
+            return True
+        elif dealer_hand.get_value() > 21:
+            print("Dealer busted. You win!!!")
+            return True
+        elif player_hand.is_blackjack() and dealer_hand.is_blackjack():
+            print("Both players have blackjack. It's a tie!")
+            return True
+        elif player_hand.is_blackjack():
+            print("You have blackjack. You win!!!")
+            return True
+        elif dealer_hand.is_blackjack():
+            print("Dealer has blackjack. Dealer wins!")
+            return True
+        return False
+    
 
 
 # Create a game instance to start playing:
